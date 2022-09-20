8448fd29c1300853cca4e537a2af35d6e9d48003
Milana-Maravic <maravic.it65.2019@uns.ac.rs>
2022-09-20 14:39:15 +0200

    Komentar

--- IT-65-2019-RVA-FrontEnd/src/app/constants.ts
+++ IT-65-2019-RVA-FrontEnd/src/app/constants.ts
@@ -1,13 +1,7 @@
-/*export const FAKULTET_URI = 'http://localhost:8083/fakultet';
-export const STATUS_URI = 'http://localhost:8083/status';
-export const DEPARTMAN_URI = 'http://localhost:8083/departman';
-export const STUDENT_URI = 'http://localhost:8083/student';
-export const STUDENT_FOR_DEPARTMAN_URI = 'http://localhost:8083/student/departman';*/
-
-export const FAKULTET_URI = 'https://backenduniverzitet.herokuapp.com/fakultet';
-export const STATUS_URI = 'https://backenduniverzitet.herokuapp.com/status';
-export const DEPARTMAN_URI = 'https://backenduniverzitet.herokuapp.com/departman';
-export const STUDENT_URI = 'https://backenduniverzitet.herokuapp.com/student';
-export const STUDENT_FOR_DEPARTMAN_URI = 'https://backenduniverzitet.herokuapp.com/student/departman';
+export const FAKULTET_URI = 'https://rva-it65-2019.herokuapp.com/fakultet';
+export const STATUS_URI = 'https://rva-it65-2019.herokuapp.com/status';
+export const DEPARTMAN_URI = 'https://rva-it65-2019.herokuapp.com/departman';
+export const STUDENT_URI = 'https://rva-it65-2019.herokuapp.com/student';
+export const STUDENT_FOR_DEPARTMAN_URI = 'https://rva-it65-2019.herokuapp.com/student/departman';
 
 
